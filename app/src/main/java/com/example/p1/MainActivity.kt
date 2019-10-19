67e5881e3b25283866dc78139a46ef18d9a8b123
jiachen725 <ongjiachen1999725@gmail.com>
2019-10-19 18:36:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/p1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.p1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var diceImage3  : ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage3 = findViewById(R.id.dice_image3)
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = Random().nextInt(6) + 1
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+    private fun rollDice() {
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+        diceImage3.setImageResource(getRandomDiceImage())
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+
+    }
+}
